eab01838654c246a605344d39ed33f0d701c6e89
Lakshman-nunna <saibajinunna3@gmail.com>
2024-02-06 21:53:31 -0600

    Made change to give multiple guesses until guessed correct

--- guess_number.py
+++ guess_number.py
@@ -3,7 +3,19 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
+
 if guess == number:
    print("Congratulations! You guessed the right number.")
 else:
-   print(f"Sorry! The number was {number}.")
+    print("No you guessed wrong, try again")
+    while guess != number:
+        guess = (input("What number am I thinking of? "))
+        if guess == 'q':
+            print(f"The number is {number}")
+            break
+        if int(guess) == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        if int(guess) != number:
+            print("No you guessed wrong, try again")
+
